ebafea5af6178ac9a81f4aafbffc2ee7c4d0a084
Lucas Borowiecki <baconomist@gmail.com>
2018-03-04 19:31:15 -0500

    Ability to remove users from database implemented.

--- modules/database_handler.py
+++ modules/database_handler.py
@@ -1,44 +1,61 @@
 import json, jsonpickle
-from modules import user
-from collections import namedtuple
-
+from modules.user import User
+import logging
 
 class DatabaseHandler(object):
+
+    databaseLocation = "../data/database.json"
+
     def __init__(self):
         try:
             self.load_data()
         except:
-            self.data = {"users": []}
-
-        self.store_user(user.User("bib", "123"))
+            self.raw_data = {"users": []}
 
     def load_data(self):
-        file = open("../data/database.json", "r+")
-        self.data = json.load(file)
+        logging.info("Loading database...")
+
+        file = open(DatabaseHandler.databaseLocation, "r+")
+        self.raw_data = json.load(file)
+
+        logging.info("Database Loaded.")
 
     def store_user(self, user):
+        logging.info("Storing user:" + str(user.name))
+
         user = jsonpickle.encode(user)
-        print(user)
-        if user not in self.data["users"]:
-            self.data["users"].append(user)
+        if user not in self.raw_data["users"]:
+            self.raw_data["users"].append(user)
         self.write()
 
+    def remove_user(self, username=""):
+
+        logging.info("Removing user:" + username)
+
+        raw_data = self.raw_data.copy()
+
+        count = 0
+        for u in raw_data["users"]:
+            if jsonpickle.decode(u).name == username:
+                self.raw_data["users"].pop(count)
+                self.write()
+                break
+            count += 1
+
     def write(self):
-        file = open("../data/database.json", "w")
-        json.dump(self.data, file, sort_keys=True, indent=4)
+        logging.info("Writing to database.")
+
+        file = open(DatabaseHandler.databaseLocation, "w")
+        json.dump(self.raw_data, file, sort_keys=True, indent=4)
 
     def get_users(self):
-        data = self.data
+        data = self.raw_data
         count = 0
-        for user in self.data["users"]:
+        for user in self.raw_data["users"]:
             data["users"][count] = jsonpickle.decode(user)
             count += 1
         return data
 
-
 databaseHandler = DatabaseHandler()
 
-print(databaseHandler.get_users())
-
-#x = json.load(databaseHandler.file, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
