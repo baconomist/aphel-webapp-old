cb0886acc365886f7794a8f7a207a172d20729ce
Lucas Borowiecki <baconomist@gmail.com>
2018-03-19 21:59:36 -0400

    For some reason 3 hours worth of commits were lost but I managed to recover the data.
    So here is the update export script which is still not dynamic and I don't like that
    but it works for lan hosting now instead of just localhost(127.0.0.1) hosting.

--- export_script/export_script.py
+++ export_script/export_script.py
@@ -3,43 +3,53 @@
 import os, sys, shutil
 from bs4 import BeautifulSoup
 
-test_dir = "C:\\Users\\Lucas\\Desktop\\test"
-#script_dir = sys.argv[0]
-#export_dir = sys.argv[1]
-export_dir = test_dir
+script_dir = sys.argv[0]
+export_dir = sys.argv[1]
+
+# test_dir = "C:\\Users\\Lucas\\Desktop\\Projects\\PCHackers\\webapp"
+# export_dir = test_dir
 
 
-if os.path.exists(export_dir + "\\templates\\"):
-    shutil.rmtree(export_dir + "\\templates\\")
+# if os.path.exists(export_dir + "\\templates\\"):
+# shutil.rmtree(export_dir + "\\templates\\")
+# os.makedirs(export_dir + "\\templates\\")
+if not os.path.exists(export_dir + "\\templates\\"):
     os.makedirs(export_dir + "\\templates\\")
-else:
-    os.makedirs(export_dir + "\\templates\\")
 
-if os.path.exists(export_dir + "\\static\\"):
-    shutil.rmtree(export_dir + "\\static\\")
+# if os.path.exists(export_dir + "\\static\\"):
+# shutil.rmtree(export_dir + "\\static\\")
+# os.makedirs(export_dir + "\\static\\")
+if not os.path.exists(export_dir + "\\static\\"):
     os.makedirs(export_dir + "\\static\\")
-else:
-    os.makedirs(export_dir + "\\static\\")
-
 
 for currentDirPath, subDirList, fileList in os.walk(export_dir):
     currentDirName = currentDirPath.split("\\")[-1]
 
-    if "templates" not in currentDirPath and "static" not in currentDirPath:
+    if "templates" not in currentDirPath and "static" not in currentDirPath and "assets" in currentDirPath or currentDirPath == export_dir:
         for filename in fileList:
             if ".html" in filename:
-                if not os.path.exists(export_dir + "\\templates\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", "")):
-                    os.makedirs(export_dir + "\\templates\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", ""))
+                if not os.path.exists(export_dir + "\\templates\\" + currentDirPath.replace(export_dir, "").replace(
+                        "\\assets\\", "")):
+                    os.makedirs(
+                        export_dir + "\\templates\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", ""))
 
-                shutil.copy(currentDirPath + "\\" + filename, export_dir + "\\templates\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", "") + "\\" + filename)
+                shutil.copy(currentDirPath + "\\" + filename,
+                            export_dir + "\\templates\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\",
+                                                                                                          "") + "\\" + filename)
 
-            elif ".css" in filename or ".js" in filename or ".png" in filename or ".jpg" in filename:
-                if not os.path.exists(export_dir + "\\static\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", "")):
-                    os.makedirs(export_dir + "\\static\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", ""))
+                os.remove(currentDirPath + "\\" + filename)
 
-                shutil.copy(currentDirPath + "\\" + filename,  export_dir + "\\static\\" + currentDirPath.replace(export_dir + "\\assets\\", "") + "\\" + filename)
+            elif ".css" in filename or ".js" in filename or ".png" in filename or ".jpg" in filename or ".eot" in filename or ".svg" in filename or ".ttf" in filename or ".woff" in filename:
+                if not os.path.exists(export_dir + "\\static\\" + currentDirPath.replace(export_dir, "").replace(
+                        "\\assets\\", "")):
+                    os.makedirs(
+                        export_dir + "\\static\\" + currentDirPath.replace(export_dir, "").replace("\\assets\\", ""))
 
-            os.remove(currentDirPath + "\\" + filename)
+                shutil.copy(currentDirPath + "\\" + filename,
+                            export_dir + "\\static\\" + currentDirPath.replace(export_dir + "\\assets\\",
+                                                                               "") + "\\" + filename)
+
+                os.remove(currentDirPath + "\\" + filename)
 
 # Remove old directories
 if os.path.exists(export_dir + "\\assets"):
@@ -48,7 +58,14 @@ if os.path.exists(export_dir + "\\assets"):
 # Auto-Template html files
 
 # Being cool ;P
-html_files = [currentDirPath + "\\" + filename for filename in fileList for currentDirPath, subDirList, fileList in os.walk(export_dir + "\\templates")]
+# html_files = [currentDirPath + "\\" + filename for filename in fileList if ".html" in filename for currentDirPath, subDirList, fileList in os.walk(export_dir + "\\templates")]
+
+html_files = []
+
+for currentDirPath, subDirList, fileList in os.walk(export_dir + "\\templates"):
+    for filename in fileList:
+        if ".html" in filename:
+            html_files.append(currentDirPath + "\\" + filename)
 
 for html_file in html_files:
     # Open file for read/write
@@ -58,18 +75,42 @@ for html_file in html_files:
 
     soup = BeautifulSoup(file, 'html.parser')
     for script in soup.findAll("script"):
-        script["src"] = "{{ url_for('static', filename=%s) }}" % (script["src"].replace("assets/", ""))
+        try:
+            script["src"] = "{{ url_for('static', filename='%s') }}" % (script["src"].replace("assets/", ""))
+        except:
+            pass
     for image in soup.findAll("img"):
         print(image)
     for link in soup.findAll("link"):
         if link["rel"][0] == "stylesheet":
-            link["href"] = "{{ url_for('static', filename=%s) }}" % (link["href"].replace("assets/", ""))
+            link["href"] = "{{ url_for('static', filename='%s') }}" % (link["href"].replace("assets/", ""))
 
     file.close()
 
     file = open(html_file, "w")
     file.write(str(soup.prettify()))
     file.close()
+
+css_files = []
+
+for currentDirPath, subDirList, fileList in os.walk(export_dir + "\\static"):
+    for filename in fileList:
+        if ".css" in filename:
+            css_files.append(currentDirPath + "\\" + filename)
+
+for css_file in css_files:
+    # Open file for read/write
+    file = open(css_file, "r")
+
+    data = file.read().replace("../assets/", "").replace("../", "/static/")  # .replace("/static/fonts/", "")
+
+    file.close()
+
+    file = open(css_file, "w")
+    file.write(data)
+    file.close()
+
+
 
 
 
--- main.py
+++ main.py
@@ -37,6 +37,6 @@ def signup():
         return request_handler.signup(request)
     return render_template("sign_up.html")
 
-
 if __name__ == "__main__":
-    app.run("127.0.0.1", 80, debug=True)
+    app.run("192.168.224.68", 80, debug=True)
+
