715a3b4088e302ee68c4fbd69d1e6689d9b30827
Lucas Borowiecki <baconomist@gmail.com>
2018-04-03 20:48:11 -0400

    Flask can now catch "*.html" links and see if it can display them.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from modules.request_handler import *
 
 import os
 import logging
+import inspect
+
 # Clear server.log
 open(os.path.join(__file__, "..\\server.log"), "w").close()
 
@@ -16,14 +18,23 @@ rootLogger.addHandler(consoleHandler)
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-@app.route("/index.html", methods=["GET"])
-def index():
-    return render_template("index.html")
+# Redirect to any html file
+@app.route("/", defaults={"path": ""})
+@app.route('/<path:path>')
+def catch_all(path):
+    if ".html" in path:
+        try:
+            return all_special_redirects[path.replace(".html", "")]()
+        except Exception:
+            return render_template(path)
+        except:
+            return "404 error not found!"
+    else:
+        return render_template("index.html")
 
 
+# Then have specific rules for specific html files/links
 @app.route("/login", methods=["GET", "POST"])
-@app.route("/login.html", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         return RequestHandler.login(request)
@@ -36,6 +47,8 @@ def signup():
         return RequestHandler.signup(request)
     return render_template("signup.html")
 
+
+all_special_redirects = {"login": login, "signup": signup}
+
 if __name__ == "__main__":
     app.run("127.0.0.1", 80, debug=True)
-
