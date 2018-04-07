3b4ed982d36b193fc45767e0a19280983fdd4c32
Lucas Borowiecki <baconomist@gmail.com>
2018-04-07 11:04:34 -0400

    simple password matching implemented on client

--- static/js/server_function/init.js
+++ static/js/server_function/init.js
@@ -3,21 +3,41 @@ var server_bridge = new ServerBridge();
 
 function apply_error_to_input(input_element, error)
 {
-    remove_success_from_input(input_element);
+    try
+    {
+        remove_success_from_input(input_element);
+    }
+    catch(err){}
+    
+    try
+    {
+        remove_error_from_input(input_element);
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+    
+    
     input_element.addClass("invalid_input");
-    input_element.before("<span class='input_error'>" + error + "</span>")
+    input_element.before("<span class='input_error' id=" + input_element.attr("id") + "_error >" + error + "</span>")
 }
 
 function apply_success_to_input(input_element)
 {
+    try
+    {
     remove_error_from_input(input_element);
+    }
+    catch(err){}
     input_element.addClass("valid_input");
 }
 
 function remove_error_from_input(input_element)
 {
     input_element.removeClass("invalid_input");
-    input_element.parent().find(".input_error").remove();
+    console.log(input_element.attr("id"));
+    input_element.parent().find("#" + input_element.attr("id") + "_error").remove();
 }
 
 function remove_success_from_input(input_element)
---
+++ static/js/server_function/signup_form.js
@@ -0,0 +1,104 @@
+// Stop button redirect,
+// return false so its as if button was not clicked
+/*$("#signup_button").on("click", function(){
+    if()
+    return false;
+});*/
+
+// When user leaves the email box
+$("#signup_form").find("#email").on("blur", function(){
+    
+    var email = $("#signup_form").find("#email").val();
+
+    server_bridge.sendToServer("/", data={"function":"user_exists", "data":email}, function(response){
+        
+        if(response["data"])
+        {
+            apply_error_to_input($("#signup_form").find("#email"), "Username already exists!");
+        }   
+        else
+        {
+            remove_error_from_input($("#signup_form").find("#email"));
+            apply_success_to_input($("#signup_form").find("#email"));
+        }
+        
+    });    
+    
+});
+
+$("#signup_form").find("#password").on("blur", function(){
+    
+    var password = $("#signup_form").find("#password").val();
+    var password_confirm = $("#signup_form").find("#password_confirm").val();
+    if(password == password_confirm)
+    {
+        apply_success_to_input($("#signup_form").find("#password"));
+        apply_success_to_input($("#signup_form").find("#password_confirm"));
+    } 
+    else
+    {
+        apply_error_to_input($("#signup_form").find("#password"), "Passwords do not match!")   
+        apply_error_to_input($("#signup_form").find("#password_confirm"), "Passwords do not match!")    
+    }
+    
+    var password = $("signup_form").find("#password").val();
+    
+    server_bridge.sendToServer("/", data={"function":"is_password_valid", "data":password}, function(response){
+        
+        if(response["data"])
+        {
+            
+        }
+        else
+        {
+                
+        }
+        
+    });
+    
+});
+
+$("#signup_form").find("#password_confirm").on("blur", function(){
+    var password = $("#signup_form").find("#password").val();
+    var password_confirm = $("#signup_form").find("#password_confirm").val();
+    if(password == password_confirm)
+    {
+        apply_success_to_input($("#signup_form").find("#password"));
+        apply_success_to_input($("#signup_form").find("#password_confirm"));
+    } 
+    else
+    {
+        apply_error_to_input($("#signup_form").find("#password"), "Passwords do not match!")   
+        apply_error_to_input($("#signup_form").find("#password_confirm"), "Passwords do not match!")    
+    }
+    
+});
+
+$("#signup_form").on("submit", function(){
+    
+    console.log("Signing up...");
+    
+    // get all the inputs into an array.
+    var inputs = $('#signup_form :input');
+    
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    inputs.each(function() {
+        values[this.id] = $(this).val();
+    });
+    
+    console.log("Password: " + values["password"] + " Email: " + values["email"]);
+    
+    //if(values["password"].length >= 8 && values["password"] == values["password_confirm"]){
+    console.log("Signing up!");
+    server_bridge.sendToServer("/signup", values, function(response){
+        console.log(response["data"]);
+    });
+    //}
+    
+    // Don't post to server/refresh the page
+    return false;
+    
+});
+  
