aa894c77d9271e0b5b9aced0e0be948cc0196e03
Lucas Borowiecki <baconomist@gmail.com>
2018-05-16 19:09:40 -0400

    Dynamic public ip

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-DEBUG = True
+from json import load
+from urllib.request import urlopen
+
+DEBUG = False
 ip = "127.0.0.1"
 port = 80
-public_address = ip + str(port)
+public_address = load(urlopen('http://jsonip.com'))['ip'] + str(port)
