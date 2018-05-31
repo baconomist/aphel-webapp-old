f8d922afc01ec2b39bcdbefe3b36732510ff7b33
Jack W <none@none>
2018-05-31 15:54:15 -0400

    Added profanity filter, still needs a frontend result if message is profane

---
+++ modules/profanity_filter.py
@@ -0,0 +1,28 @@
+import os
+
+
+class ProfanityFilter(object):
+    profane_words = []
+    chars_to_remove = [" ", ".", ","]
+
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), "..", "data", "profane_words.txt"), "r") as profane_words_file:
+            self.profane_words = profane_words_file.read().split("\n")
+        print(self.profane_words)
+
+    def is_profane(self, text):
+        # Convert to lowercase
+        no_spaces = text.lower()
+
+        for c in self.chars_to_remove:
+            no_spaces = no_spaces.replace(c, "")
+
+        print(no_spaces)
+
+        for i in self.profane_words:
+            if i in no_spaces:
+                return True
+            else:
+                pass
+
+        return False
