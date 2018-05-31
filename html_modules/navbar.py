e0a6bfe62c5fe965482091624a35b1226ef43d2f
Lucas Borowiecki <baconomist@gmail.com>
2018-05-30 22:00:50 -0400

    Having navbar as instance was dumb, just have one variable reference.

--- html_modules/navbar.py
+++ html_modules/navbar.py
@@ -2,6 +2,4 @@ import os
 
 from flask import Markup
 
-class NavBar(object):
-    def __init__(self):
-        self.markup = Markup(open(os.path.join(os.path.dirname(__file__), "navbar.html"), "r").read())
+navbar_markup = Markup(open(os.path.join(os.path.dirname(__file__), "navbar.html"), "r").read())
