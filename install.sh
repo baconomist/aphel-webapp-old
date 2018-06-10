d7689f9fbc33bedb7bc44b84056457d819ef46a9
Lucas Borowiecki <baconomist@gmail.com>
2018-06-10 14:08:13 -0400

    Updated start.sh
    Added install.sh(haven't tested)
    
    -Using python3.6 on linux now
    
    TODO:
        Setup auto-pulls to server on linux from a "release" branch

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+
+sudo apt-get install python3.6
+sudo apt-get install python3.6-dev
+sudo apt-get install python3.6-venv
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.6 get-pip.py
+
+virtualenv /home/webapp/webappenv/
+
+source /home/webapp/start.sh
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 sudo kill `sudo lsof -t -i:80`
+
+source /home/webapp/webappenv/bin/activate
+pip install -r /home/webapp/requirements.txt
+
 uwsgi --socket 0.0.0.0:80 --protocol=http -w wsgi
